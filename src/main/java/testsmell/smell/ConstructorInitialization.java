1ab1d59a49c61d1128cd414c15d8ec88c6a12914
shehan peruma <shehan.peruma@gmail.com>
2017-06-27 12:11:01 +0530

    removed commented out code

--- src/main/java/testsmell/smell/ConstructorInitialization.java
+++ src/main/java/testsmell/smell/ConstructorInitialization.java
@@ -14,7 +14,7 @@ import java.util.List;
 This class checks if the code file contains a Constructor. Ideally, the test suite should not have a constructor. Initialization of fields should be in the setUP() method
 If this code detects the existence, it sets the class as smelly
  */
-public class ConstructorInitialization implements ITestSmell{
+public class ConstructorInitialization implements ITestSmell {
     List<ISmell> smellList;
 
 
@@ -33,11 +33,9 @@ public class ConstructorInitialization implements ITestSmell{
 
         @Override
         public void visit(ConstructorDeclaration n, Void arg) {
-            //if(n.getBody().getStatements().size()>=1){ //uncomment this condition if we need to check if we need to indicate a smell only if the constructor has code statements in it
-               classSmell = new ClassSmell(n.getNameAsString());
-               classSmell.setHasSmell(true);
-               smellList.add(classSmell);
-            //}
+            classSmell = new ClassSmell(n.getNameAsString());
+            classSmell.setHasSmell(true);
+            smellList.add(classSmell);
         }
     }
 }
