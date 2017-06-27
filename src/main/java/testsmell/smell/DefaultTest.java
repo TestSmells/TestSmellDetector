1f04e025c81e31b6afcc5c57d602db65dcbe3a2e
shehan peruma <shehan.peruma@gmail.com>
2017-06-27 13:41:14 +0530

    added comment describing the class

--- src/main/java/testsmell/smell/DefaultTest.java
+++ src/main/java/testsmell/smell/DefaultTest.java
@@ -8,7 +8,10 @@ import testsmell.ISmell;
 
 import java.util.ArrayList;
 import java.util.List;
-
+/*
+By default Android Studio creates default test classes when a project is created. These classes are meant to serve as an example for developers when wring unit tests
+This code marks the class as smelly if the class name corresponds to the name of the default test classes
+ */
 public class DefaultTest implements ITestSmell {
 
     List<ISmell> smellList;
