3afc3ac59cd4cb5ed6399432c9cf2d5a1a60e4f0
shehan peruma <shehan.peruma@gmail.com>
2017-06-28 21:50:35 +0530

    updated

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,13 +1,49 @@
+import testsmell.ResultsWriter;
 import testsmell.TestSmellDetector;
 
-import java.io.FileNotFoundException;
+import java.io.BufferedReader;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
 
 public class Main {
-    public static void main(String[] args) throws FileNotFoundException {
-        TestSmellDetector testSmellDetector = new TestSmellDetector();
+    public static void main(String[] args) throws IOException {
+        TestSmellDetector testSmellDetector = TestSmellDetector.createTestSmellDetector();
+
+        BufferedReader in = new BufferedReader(new FileReader("G:\\Tools\\TestSmellDetector\\testFiles.csv"));
+        String str;
+
+        ResultsWriter resultsWriter = ResultsWriter.createResultsWriter();
+        List<String> columnNames = null;
+        List<String> columnValues = null;
+
+        Map<String,String> data;
+        while((str = in.readLine()) != null){
+            data = testSmellDetector.detectSmells(str);
+
+            if(columnNames==null){
+                columnNames = testSmellDetector.getTestSmellName();
+                columnNames.add(0,"FilePath");
+                resultsWriter.writeColumnName(columnNames);
+            }
+
+            columnValues = new ArrayList<>();
+            for (String column: columnNames) {
+                columnValues.add(data.get(column));
+            }
+            resultsWriter.writeLine(columnValues);
+
+
+        }
+
+
+
+
         //testSmellDetector.detectSmells("C:\\Projects\\TestSmells_ExisitngTools\\samples\\ar.rulosoft.mimanganu\\app\\src\\androidTest\\java\\ar\\rulosoft\\mimanganu\\TestServers.java");
-        //testSmellDetector.detectSmells("C:\\Projects\\TestSmells_ExisitngTools\\samples\\org.xbmc.kore\\app\\src\\test\\java\\org\\xbmc\\kore\\provider\\mediaprovider\\AlbumsTest.java");
-        testSmellDetector.detectSmells("C:\\Projects\\TestSmells\\TestSmellDetector\\src\\main\\java\\testsmell\\smell\\EmptyTest.java");
+        //testSmellDetector.detectSmells("C:\\Projects\\TestSmells\\HashOperationPresenterTest.java");
+
         System.out.println("end");
     }
 }
