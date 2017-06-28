614edcc200299f233ec7fcdeee2af5bba1fa3dcc
shehan peruma <shehan.peruma@gmail.com>
2017-06-28 20:50:17 +0530

    refactored the class

--- src/main/java/testsmell/TestSmellDetector.java
+++ src/main/java/testsmell/TestSmellDetector.java
@@ -5,33 +5,80 @@ import com.github.javaparser.ast.CompilationUnit;
 import testsmell.smell.*;
 
 import java.io.FileInputStream;
-import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
 
 public class TestSmellDetector {
 
-    private ITestSmell ar,se,cl,mg,dt,et;
+    private List<ITestSmell> testSmells;
 
-    public TestSmellDetector(){
-        ar = new AssertionRoulette();
-        se = new SensitiveEquality();
-        cl = new ConditionalTestLogic();
-        mg = new MysteryGuest();
-        dt = new DefaultTest();
-        et = new EmptyTest();
+    /**
+     * Instantiates the various test smell analyzer classes and loads the objects into an List
+     */
+    private TestSmellDetector() {
 
+        testSmells = new ArrayList<>();
+        testSmells.add(new AssertionRoulette());
+        testSmells.add(new ConditionalTestLogic());
+        testSmells.add(new ConstructorInitialization());
+        testSmells.add(new DefaultTest());
+        testSmells.add(new EmptyTest());
+        testSmells.add(new ExceptionCatchingThrowing());
+        testSmells.add(new GeneralFixture());
+        testSmells.add(new MysteryGuest());
+        testSmells.add(new PrintStatement());
+        testSmells.add(new RedundantAssertion());
+        testSmells.add(new SensitiveEquality());
+        testSmells.add(new VerboseTest());
+        testSmells.add(new WaitAndSee());
     }
 
-    public void detectSmells(String absoluteFilePath) throws FileNotFoundException {
-        if(absoluteFilePath.length()!=0){
-            FileInputStream fTemp = new FileInputStream(absoluteFilePath);
-            CompilationUnit compilationUnit = JavaParser.parse(fTemp);
+    /**
+     * Factory method that provides a new instance of the TestSmellDetector
+     * @return new TestSmellDetector instance
+     */
+    public static TestSmellDetector createTestSmellDetector() {
+        return new TestSmellDetector();
+    }
 
-            //System.out.println("AssertionRoulette: "+ar.runAnalysis(compilationUnit).size());
-            //System.out.println("ConditionalTestLogic: "+cl.runAnalysis(compilationUnit).size());
-            //System.out.println("SE: "+se.runAnalysis(compilationUnit).size());
-            //System.out.println("MG: "+mg.runAnalysis(compilationUnit).size());
-            System.out.println("DT: "+dt.runAnalysis(compilationUnit).size());
+    /**
+     * Provides the names of the smells that are being checked for in the code
+     * @return list of smell names
+     */
+    public List<String> getTestSmellName() {
+        return testSmells.stream().map(p -> p.getSmellNameAsString()).collect(Collectors.toList());
+    }
+
+    /**
+     * Loads the java source code file into an AST and then analyzes it for the existence of the different types of test smells
+     * @param absoluteFilePath  the name of the source code that is to be analyzed for test smells
+     * @return  a dictionary indicating if a smell occurs or not
+     * @throws IOException
+     */
+    public Map<String, String> detectSmells(String absoluteFilePath) throws IOException {
+        Map<String, String> analysisResult = null;
+
+        if (absoluteFilePath.length() != 0) {
+            FileInputStream fileInputStream = new FileInputStream(absoluteFilePath);
+            CompilationUnit compilationUnit = JavaParser.parse(fileInputStream);
+
+            analysisResult = new HashMap<>();
+            analysisResult.put("FilePath", absoluteFilePath);
+
+            for (ITestSmell smell : testSmells) {
+                if (smell.runAnalysis(compilationUnit).stream().filter(x -> x.isHasSmell()).collect(Collectors.toList()).size() >= 1) {
+                    analysisResult.put(smell.getSmellNameAsString(), "true");
+                } else {
+                    analysisResult.put(smell.getSmellNameAsString(), "false");
+                }
+            }
         }
+
+        return analysisResult;
 
     }
 
