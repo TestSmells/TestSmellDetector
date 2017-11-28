3d9828d1c36c666bdb35a3a303f925530bb84016
shehan <shehan.peruma@gmail.com>
2017-11-28 19:47:57 +0000

    get the path of the input file as an argument from the commandline (instead of hardcoding)

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -4,6 +4,7 @@ import testsmell.TestFile;
 import testsmell.TestSmellDetector;
 
 import java.io.BufferedReader;
+import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
 import java.text.DateFormat;
@@ -14,12 +15,25 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        if (args == null) {
+            System.out.println("Please provide the file containing the paths to the collection of test files");
+            return;
+        }
+        if(!args[0].isEmpty()){
+            File inputFile = new File(args[0]);
+            if(!inputFile.exists() || inputFile.isDirectory()) {
+                System.out.println("Please provide a valid file containing the paths to the collection of test files");
+                return;
+            }
+        }
+
+
         TestSmellDetector testSmellDetector = TestSmellDetector.createTestSmellDetector();
 
         /*
           Read the input file and build the TestFile objects
          */
-        BufferedReader in = new BufferedReader(new FileReader("C:\\Projects\\TestSmells\\testing\\allTests.txt"));
+        BufferedReader in = new BufferedReader(new FileReader(args[0]));
         String str;
 
         String[] lineItem;
