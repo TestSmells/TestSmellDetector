50d6b9e8485f8b546351382457b2b774433a3fc7
shehan <shehan.peruma@gmail.com>
2017-12-22 20:44:21 +0530

    Utility class. Added functionality to check if a MethodDeclaration is a valid  test method

---
+++ src/main/java/testsmell/Util.java
@@ -0,0 +1,23 @@
+package testsmell;
+
+import com.github.javaparser.ast.Modifier;
+import com.github.javaparser.ast.body.MethodDeclaration;
+
+public class Util {
+
+    public static boolean isValidTestMethod(MethodDeclaration n) {
+        boolean valid = false;
+
+        if (!n.getAnnotationByName("Ignore").isPresent()) {
+            //only analyze methods that either have a @test annotation (Junit 4) or the method name starts with 'test'
+            if (n.getAnnotationByName("Test").isPresent() || n.getNameAsString().toLowerCase().startsWith("test")) {
+                //must be a public method
+                if (n.getModifiers().contains(Modifier.PUBLIC)) {
+                    valid = true;
+                }
+            }
+        }
+
+        return valid;
+    }
+}
