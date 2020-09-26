f5b317893ce58f627ba0b2b423b278fa3752eb50
Victor Guerra Veloso <victorgvbh@gmail.com>
2020-09-26 05:50:08 -0300

    Fixed some warnings

--- src/test/java/edu/rit/se/testsmells/testsmell/ReportControllerIntegrationTest.java
+++ src/test/java/edu/rit/se/testsmells/testsmell/ReportControllerIntegrationTest.java
@@ -11,24 +11,25 @@ import java.io.FileReader;
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
 import static org.junit.jupiter.api.Assertions.*;
 
 @IntegrationTest
-public class ReportControllerIntegrationTest {
-    File outputFile;
-    TestFile file;
-    ReportController sut;
-    ResultsWriter resultsWriter;
-    List<AbstractSmell> smells = Arrays.asList(new AssertionRoulette(), new ConditionalTestLogic(), new ConstructorInitialization(), new DefaultTest(), new EmptyTest(), new ExceptionCatchingThrowing(), new GeneralFixture(), new MysteryGuest(), new PrintStatement(), new RedundantAssertion(), new SensitiveEquality(), new VerboseTest(), new SleepyTest(), new EagerTest(), new LazyTest(), new DuplicateAssert(), new UnknownTest(), new IgnoredTest(), new ResourceOptimism(), new MagicNumberTest(), new DependentTest());
-    String appName = "LazyTest";
-    String testFilePath = "/LazyTest/src/test/java/com/github/marmaladesky/tests/CryptographerTest.java";
-    String productionFilePath = "/LazyTest/src/main/java/com/github/marmaladesky/Cryptographer.java";
+class ReportControllerIntegrationTest {
+    private File outputFile;
+    private TestFile file;
+    private ReportController sut;
+    private ResultsWriter resultsWriter;
+    private List<AbstractSmell> smells = Arrays.asList(new AssertionRoulette(), new ConditionalTestLogic(), new ConstructorInitialization(), new DefaultTest(), new EmptyTest(), new ExceptionCatchingThrowing(), new GeneralFixture(), new MysteryGuest(), new PrintStatement(), new RedundantAssertion(), new SensitiveEquality(), new VerboseTest(), new SleepyTest(), new EagerTest(), new LazyTest(), new DuplicateAssert(), new UnknownTest(), new IgnoredTest(), new ResourceOptimism(), new MagicNumberTest(), new DependentTest());
+    private String appName = "LazyTest";
+    private String testFilePath = "/LazyTest/src/test/java/com/github/marmaladesky/tests/CryptographerTest.java";
+    private String productionFilePath = "/LazyTest/src/main/java/com/github/marmaladesky/Cryptographer.java";
 
     @BeforeEach
-    public void setUp() throws IOException {
+    void setUp() throws IOException {
         TestSmellDetector testSmellDetector = TestSmellDetector.createTestSmellDetector();
         smells.forEach(testSmellDetector::addDetectableSmell);
         file = new TestFile(appName, testFilePath, productionFilePath);
@@ -39,20 +40,21 @@ public class ReportControllerIntegrationTest {
     }
 
     @AfterEach
-    public void tearDown() {
+    void tearDown() {
+        //noinspection ResultOfMethodCallIgnored
         outputFile.delete();
     }
 
     @Test
-    public void testReport() throws IOException {
-        sut = new ReportController(resultsWriter, Arrays.asList(ReportController.ReportGranularity.FILE));
-        assertDoesNotThrow(() -> sut.report(Arrays.asList(file)));
+    void testReport() {
+        sut = new ReportController(resultsWriter, Collections.singletonList(ReportController.ReportGranularity.FILE));
+        assertDoesNotThrow(() -> sut.report(Collections.singletonList(file)));
     }
 
     @Test
-    public void testHeader() throws IOException {
-        sut = new ReportController(resultsWriter, Arrays.asList(ReportController.ReportGranularity.FILE));
-        sut.report(Arrays.asList(file));
+    void testHeader() throws IOException {
+        sut = new ReportController(resultsWriter, Collections.singletonList(ReportController.ReportGranularity.FILE));
+        sut.report(Collections.singletonList(file));
         assertTrue(outputFile.exists(), "Output file missing!");
 
         List<String> headerEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).readLine().split(","));
@@ -65,12 +67,12 @@ public class ReportControllerIntegrationTest {
     }
 
     @Test
-    public void testContent_FILE() throws IOException {
-        sut = new ReportController(resultsWriter, Arrays.asList(ReportController.ReportGranularity.FILE));
-        sut.report(Arrays.asList(file));
+    void testContent_FILE() throws IOException {
+        sut = new ReportController(resultsWriter, Collections.singletonList(ReportController.ReportGranularity.FILE));
+        sut.report(Collections.singletonList(file));
         assertTrue(outputFile.exists(), "Output file missing!");
 
-        List<String> contentEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).lines().skip(1).findFirst().get().split(","));
+        List<String> contentEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).lines().skip(1).findFirst().orElse("").split(","));
 
         List<String> expectedEntries = new ArrayList<>(Arrays.asList(appName, "Cryptographer.java", testFilePath, "CryptographerTest.java", "src/main/java/com/github/marmaladesky/Cryptographer.java", "src/test/java/com/github/marmaladesky/tests/CryptographerTest.java", productionFilePath));
 
@@ -94,37 +96,37 @@ public class ReportControllerIntegrationTest {
     }
 
     @Test
-    public void testContent_METHOD_Decrypt() throws IOException {
-        sut = new ReportController(resultsWriter, Arrays.asList(ReportController.ReportGranularity.METHOD));
-        sut.report(Arrays.asList(file));
+    void testContent_METHOD_Decrypt() throws IOException {
+        sut = new ReportController(resultsWriter, Collections.singletonList(ReportController.ReportGranularity.METHOD));
+        sut.report(Collections.singletonList(file));
 
         assertTrue(outputFile.exists(), "Output file missing!");
 
         assertEquals(17, getMethodSmells().size());
 
-        List<String> contentEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).lines().skip(1).findFirst().get().split(","));
+        List<String> contentEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).lines().skip(1).findFirst().orElse("").split(","));
         assertEquals(2, contentEntries.stream().filter(e -> e.equals("true")).count()); // "Lazy Test" and "Exception Catching Throwing"
         assertEquals(15, contentEntries.stream().filter(e -> e.equals("false")).count()); // 17 methods smells - 2 detected smells
     }
 
     @Test
-    public void testContent_METHOD_Encrypt() throws IOException {
-        sut = new ReportController(resultsWriter, Arrays.asList(ReportController.ReportGranularity.METHOD));
-        sut.report(Arrays.asList(file));
+    void testContent_METHOD_Encrypt() throws IOException {
+        sut = new ReportController(resultsWriter, Collections.singletonList(ReportController.ReportGranularity.METHOD));
+        sut.report(Collections.singletonList(file));
 
         assertTrue(outputFile.exists(), "Output file missing!");
 
         assertEquals(17, getMethodSmells().size());
 
-        List<String> contentEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).lines().skip(2).findFirst().get().split(","));
+        List<String> contentEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).lines().skip(2).findFirst().orElse("").split(","));
         assertEquals(3, contentEntries.stream().filter(e -> e.equals("true")).count()); // "Eager Test", "Lazy Test" and "Exception Catching Throwing"
         assertEquals(14, contentEntries.stream().filter(e -> e.equals("false")).count()); // 17 methods smells - 3 detected smells
     }
 
     @Test
-    public void testConsistentColumns() throws IOException {
-        sut = new ReportController(resultsWriter, Arrays.asList(ReportController.ReportGranularity.FILE));
-        sut.report(Arrays.asList(file));
+    void testConsistentColumns() throws IOException {
+        sut = new ReportController(resultsWriter, Collections.singletonList(ReportController.ReportGranularity.FILE));
+        sut.report(Collections.singletonList(file));
         assertTrue(outputFile.exists(), "Output file missing!");
 
         BufferedReader content = new BufferedReader(new FileReader(outputFile));
