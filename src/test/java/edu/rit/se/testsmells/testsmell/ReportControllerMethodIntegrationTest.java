daecf808b17f2636ad3fc63f8adb5638cfb7271c
Victor Guerra Veloso <victorgvbh@gmail.com>
2020-09-28 11:47:59 -0300

    Enhanced ReportControllerIntegrationTests organization even more!

--- src/test/java/edu/rit/se/testsmells/testsmell/ReportControllerMethodIntegrationTest.java
+++ src/test/java/edu/rit/se/testsmells/testsmell/ReportControllerMethodIntegrationTest.java
@@ -15,10 +15,11 @@ import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 @IntegrationTest
-class ReportControllerIntegrationTest {
+class ReportControllerMethodIntegrationTest {
     private File outputFile;
     private TestFile file;
     private ReportController sut;
@@ -35,6 +36,9 @@ class ReportControllerIntegrationTest {
         file = new TestFile(appName, testFilePath, productionFilePath);
         csvWriter = CSVWriter.createResultsWriter();
         testSmellDetector.detectSmells(file);
+        sut = new ReportController(csvWriter, Collections.singletonList(ReportController.ReportGranularity.METHOD));
+        sut.report(Collections.singletonList(file));
+        outputFile = new File(csvWriter.getFilename());
     }
 
     @AfterEach
@@ -44,45 +48,8 @@ class ReportControllerIntegrationTest {
     }
 
     @Test
-    void testReport() {
-        sut = new ReportController(csvWriter, Collections.singletonList(ReportController.ReportGranularity.FILE));
-        assertDoesNotThrow(() -> sut.report(Collections.singletonList(file)));
-        outputFile = new File(csvWriter.getFilename());
-    }
-
-    @Test
-    void testHeader_FILE() throws IOException {
-        sut = new ReportController(csvWriter, Collections.singletonList(ReportController.ReportGranularity.FILE));
-        sut.report(Collections.singletonList(file));
-        outputFile = new File(csvWriter.getFilename());
+    void testReportExists() {
         assertTrue(outputFile.exists(), "Output file missing!");
-
-        List<String> headerEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).readLine().split(","));
-
-        List<String> expectedEntries = new ArrayList<>(Arrays.asList("App", "ProductionFileName", "TestFilePath", "TestFileName", "RelativeProductionFilePath", "RelativeTestFilePath", "ProductionFilePath"));
-        List<String> smellsName = smells.stream().map(AbstractSmell::getSmellName).collect(Collectors.toList());
-        expectedEntries.addAll(smellsName);
-
-        assertIterableEquals(headerEntries, expectedEntries);
-    }
-
-    @Test
-    void testContent_FILE() throws IOException {
-        sut = new ReportController(csvWriter, Collections.singletonList(ReportController.ReportGranularity.FILE));
-        sut.report(Collections.singletonList(file));
-        outputFile = new File(csvWriter.getFilename());
-        assertTrue(outputFile.exists(), "Output file missing!");
-
-        List<String> contentEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).lines().skip(1).findFirst().orElse("").split(","));
-
-        List<String> expectedEntries = new ArrayList<>(Arrays.asList(appName, "Cryptographer.java", testFilePath, "CryptographerTest.java", "src/main/java/com/github/marmaladesky/Cryptographer.java", "src/test/java/com/github/marmaladesky/tests/CryptographerTest.java", productionFilePath));
-
-        List<String> expectedSmells = Arrays.asList("Lazy Test", "Eager Test", "Exception Catching Throwing");
-        List<String> hasSmell = smells.stream().map(x -> expectedSmells.contains(x.getSmellName()) ? "true" : "false").collect(Collectors.toList());
-
-        expectedEntries.addAll(hasSmell);
-
-        assertIterableEquals(expectedEntries, contentEntries);
     }
 
     List<AbstractSmell> getMethodSmells() {
@@ -98,11 +65,6 @@ class ReportControllerIntegrationTest {
 
     @Test
     void testReport_METHOD() throws IOException {
-        sut = new ReportController(csvWriter, Collections.singletonList(ReportController.ReportGranularity.METHOD));
-        sut.report(Collections.singletonList(file));
-        outputFile = new File(csvWriter.getFilename());
-        assertTrue(outputFile.exists(), "Output file missing!");
-
         List<String> entries = new BufferedReader(new FileReader(outputFile)).lines().collect(Collectors.toList());
 
 
@@ -113,11 +75,6 @@ class ReportControllerIntegrationTest {
 
     @Test
     void testHeader_METHOD() throws IOException {
-        sut = new ReportController(csvWriter, Collections.singletonList(ReportController.ReportGranularity.METHOD));
-        sut.report(Collections.singletonList(file));
-        outputFile = new File(csvWriter.getFilename());
-        assertTrue(outputFile.exists(), "Output file missing!");
-
         List<String> headerEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).readLine().split(","));
 
         assertEquals("Element Name,WhileCount,ConditionCount,RedundantCount,AssertCount,IfCount,ExceptionCount,ForeachCount,PrintCount,SwitchCount,MysteryCount,ForCount,VerboseCount,ResourceOptimismCount,ThreadSleepCount,SensitiveCount,MagicNumberCount,Assertion Roulette,Eager Test,Mystery Guest,Sleepy Test,Unknown Test,Redundant Assertion,Magic Number Test,Conditional Test Logic,EmptyTest,General Fixture,Sensitive Equality,Verbose Test,Resource Optimism,Duplicate Assert,Exception Catching Throwing,Print Statement,Lazy Test", String.join(",", headerEntries)/*, String.join(",",expectedEntries)*/);
@@ -125,12 +82,6 @@ class ReportControllerIntegrationTest {
 
     @Test
     void testContent_METHOD_Decrypt() throws IOException {
-        sut = new ReportController(csvWriter, Collections.singletonList(ReportController.ReportGranularity.METHOD));
-        sut.report(Collections.singletonList(file));
-        outputFile = new File(csvWriter.getFilename());
-
-        assertTrue(outputFile.exists(), "Output file missing!");
-
         assertEquals(17, getMethodSmells().size());
 
         List<String> contentEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).lines().skip(1).findFirst().orElse("").split(","));
@@ -140,31 +91,11 @@ class ReportControllerIntegrationTest {
 
     @Test
     void testContent_METHOD_Encrypt() throws IOException {
-        sut = new ReportController(csvWriter, Collections.singletonList(ReportController.ReportGranularity.METHOD));
-        sut.report(Collections.singletonList(file));
-        outputFile = new File(csvWriter.getFilename());
-
-        assertTrue(outputFile.exists(), "Output file missing!");
-
         assertEquals(17, getMethodSmells().size());
 
         List<String> contentEntries = Arrays.asList(new BufferedReader(new FileReader(outputFile)).lines().skip(2).findFirst().orElse("").split(","));
         assertEquals(3, contentEntries.stream().filter(e -> e.equals("true")).count()); // "Eager Test", "Lazy Test" and "Exception Catching Throwing"
         assertEquals(14, contentEntries.stream().filter(e -> e.equals("false")).count()); // 17 methods smells - 3 detected smells
-    }
-
-    @Test
-    void testConsistentColumns() throws IOException {
-        sut = new ReportController(csvWriter, Collections.singletonList(ReportController.ReportGranularity.FILE));
-        sut.report(Collections.singletonList(file));
-        outputFile = new File(csvWriter.getFilename());
-        assertTrue(outputFile.exists(), "Output file missing!");
-
-        BufferedReader content = new BufferedReader(new FileReader(outputFile));
-        int nCols = content.readLine().split(",").length;
-
-        assertEquals(nCols, content.readLine().split(",").length);
-        assertEquals(nCols, 7 + smells.size());
     }
 }
 
